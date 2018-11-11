e234284bb7a31aa5f414083f3643103c79ebd7a1
sunyifeng <9339996@qq.com>
2018-11-11 21:00:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# rabbitmq-in-action-php
---
+++ ch04-rabbitmq-example/listing-4.7.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunyifeng
+ * Date: 2018/11/11
+ * Time: 8:43 PM
+ */
+
+require_once('../lib/php-amqplib/amqp.inc');
+require_once('../config/config.php');
+
+$conn = new AMQPConnection(HOST, PORT, USER, PASS, VHOST);
+$channel = $conn->channel();
+
+$channel->exchange_declare('upload-pictures',
+    'fanout', false, true, false);
+
+$metadata = json_encode(array(
+    'image_id' => $image_id,
+    'user_id' => $user_id,
+    'image_path' => $image_path
+));
+
+$msg = new AMQPMessage($metadata,
+    array('content_type' => 'application/json',
+        'delivery_mode' => 2));
+
+$channel->basic_publish($msg, 'upload-pictures');
+
+$channel->close();
+$conn->close();
+
+?>
